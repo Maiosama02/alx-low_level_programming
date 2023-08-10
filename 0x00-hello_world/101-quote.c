d6c348296ae62308f629c3d5c3bec325ffb0251b
Maiosama02 <mai.karoom02@gmail.com>
2023-08-10 12:45:06 -0700

    cp

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,12 @@
 /**
  * main - entry point
  * description : C program
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	write
+	char guo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
