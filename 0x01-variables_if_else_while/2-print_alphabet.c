0ba9df29aff6a8962df73011cf44ab169869871c
Maiosama02 <mai.karoom02@gmail.com>
2023-08-11 11:29:08 -0700

    cv

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'c program'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
