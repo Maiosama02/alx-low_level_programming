cf8d16b0a7d9bf7d4bf1d259577abe8a47cfc074
Maiosama02 <mai.karoom02@gmail.com>
2023-08-11 13:50:12 -0700

    cv

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'c program'
+ * Return: always 0
+ */
+int main(void)
+
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+	printf("%d", i);
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
