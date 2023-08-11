db781df89dd4f624569018f532abeb2e30623789
Maiosama02 <mai.karoom02@gmail.com>
2023-08-11 12:03:46 -0700

    cv

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'c program'
+ * Return: always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	putchar(c);
+	putchar(c - 32);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
