ee4423cfbbd8cdfc02d17f894d34b6e419179fa9
Maiosama02 <mai.karoom02@gmail.com>
2023-08-11 14:25:45 -0700

    cv

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'c program'
+ * Return: always 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+	putchar(c);
+	c--;
+	}
+	putchar('\n');
+	return (0);
+}
