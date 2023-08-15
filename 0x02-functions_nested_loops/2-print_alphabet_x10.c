18e6de6d3e088c843f88063f3061fc327a777d1c
Maiosama02 <mai.karoom02@gmail.com>
2023-08-15 12:10:12 -0700

    cf

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	i = 0;
+	j = 'a';
+	while (i < 10)
+	{
+		while (j <= 'z')
+		{
+		_putchar(j);
+		j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
