235aa7d01165b0571f4d0fa534204cfcd8187351
Maiosama02 <mai.karoom02@gmail.com>
2023-08-15 13:31:04 -0700

    cf

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: character in
+ * Return: 1 if c is lowercase , 0 if not
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
