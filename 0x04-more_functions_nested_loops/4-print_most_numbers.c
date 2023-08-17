d6432f7b504625ee62b85f46e2d0b85ae5396d9c
Maiosama02 <mai.karoom02@gmail.com>
2023-08-17 06:49:26 -0700

    more nl

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,9 @@ void print_most_numbers(void)
 
 	for (i = 48; i <= 58; i++)
 	{
-		if (i != 50 && i != 52)
-			_putchar(i + 48);
+		if (i == 50 || i == 52)
+			continue
+		_putchar(i + 48);
 	}
 	_putchar('\n');
 }
