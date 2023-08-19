4c5085a697865aa2f0a94b7cc9320c34267eac04
Maiosama02 <mai.karoom02@gmail.com>
2023-08-18 17:50:34 -0700

    more nl

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(48 + (i % 10));
+}
