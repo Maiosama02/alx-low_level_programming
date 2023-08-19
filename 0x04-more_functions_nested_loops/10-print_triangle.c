d6a937568cfcd35aa68605ad7caf489efd211600
Maiosama02 <mai.karoom02@gmail.com>
2023-08-18 18:00:36 -0700

    more nl

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,18 +8,23 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
-	if (size > 0)
-	{
 	for (i = 0; i < size; i++)
 	{
-	for (j = 0; j < i; j++)
+	for (j = 0; j < size; j++)
 	{
+		if (j < size -m)
+		{
+			_putchar(" ");
+		}
+		else
+		{
 		_putchar('#');
+		}
 	}
 	_putchar('\n');
-	}
+	m++;
 	}
 	if (size <= 0)
 	{
