93f07dedf8844d172518f2827303f74f4ba45e89
Maiosama02 <mai.karoom02@gmail.com>
2023-08-21 09:27:34 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: string
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
