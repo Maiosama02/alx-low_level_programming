29282681946eb6368f316df3a1a2e48dba2190d1
Maiosama02 <mai.karoom02@gmail.com>
2023-08-23 17:12:22 -0700

    strings,arrays,pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *  *cap_string - capitalizes all words of a string
+ *  @s: string
+ *  Return: s
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	while (s[i] != '\0')
+	{
+		if (i == 0 || strchr(sep, s[i - 1]))
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
