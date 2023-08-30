d2c19c7fe58892ef94b54e6f4bab4d996e168d29
Maiosama02 <mai.karoom02@gmail.com>
2023-08-30 15:10:48 -0700

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks for prime numbers
+ * @n: number
+ * Return: 1 if priime number , 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (n == 1 && n == 0)
+	{
+		return (0);
+	}
+	if (n == i)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+	return (is_prime_number(n));
+}
