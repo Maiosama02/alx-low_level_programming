4d3ac4c8bfe50520b6cce54e6b69dbf303b15f12
Maiosama02 <mai.karoom02@gmail.com>
2023-09-05 07:20:14 -0700

    memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i, j;
 
 	if (s1 == NULL)
-		return (NULL);
+		s1 = "";
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 
 	len1 = 0;
 	while (s1[len1] != '\0')
