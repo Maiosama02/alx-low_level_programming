348ccf63ebb0a0c485e9cbf298ad53b5d8226076
Maiosama02 <mai.karoom02@gmail.com>
2023-09-07 12:26:40 -0700

    malloc & exit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,10 +29,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	new_ptr = malloc(new_sie);
+	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
-	old_ptr = ptr
+	old_ptr = ptr;
 		for (i = 0; i < old_size && i < new_size; i++)
 			new_ptr[i] = old_ptr[i];
 	free(ptr);
