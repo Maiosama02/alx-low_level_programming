0116a6de7d39b94d0902b500324e4b13a9642d8e
Maiosama02 <mai.karoom02@gmail.com>
2023-09-11 15:22:54 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct
+ * @name: dog name
+ * @age: doge age
+ * @owner: dog owner
+ * Description: a dog struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
