db2067acd711332d2a173b90429250f0f49999f0
Maiosama02 <mai.karoom02@gmail.com>
2023-09-13 10:23:43 -0700

    functions pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum
+ * @a: input
+ * @b: input
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference
+ * @a: input
+ * @b: input
+ * Return: sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product
+ * @a: input
+ * @b: input
+ * Return: multi
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the division
+ * @a: input
+ * @b: input
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division
+ * @a: input
+ * @b: input
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
