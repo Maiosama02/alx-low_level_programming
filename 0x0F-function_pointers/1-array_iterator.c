531febec4154209e6937311a7a479681eabbfb8f
Maiosama02 <mai.karoom02@gmail.com>
2023-09-13 06:31:18 -0700

    functions pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,17 +5,18 @@
  * @array: array
  * @size: size of an array
  * @action: pointer to function
+ * Return: nothing
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (array && action)
-	{
+	if (array == NULL || action == NULL)
+		return;
+
 		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
-	}
 }
