ee96ef20993265748eaa8355b1bf66f9f0bc8155
Maiosama02 <mai.karoom02@gmail.com>
2023-09-28 16:17:35 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,12 +15,12 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL || *b == '\0')
 		return (0);
 
-	while (b[i] != '\0')
+	for (i = 0; b[i]; i++)
 	{
-	if (b[i] != '\0' && b[i] != '1')
-		return (0);
-	num = (num << 1) + (b[i] - '\0');
-	i++;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		num = 2 * num + (b[i] - '0');
 	}
 	return (num);
+
 }
