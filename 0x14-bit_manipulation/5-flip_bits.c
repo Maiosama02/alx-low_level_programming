673899dec1aaad374e58514fe0899a3112695ee1
Maiosama02 <mai.karoom02@gmail.com>
2023-09-28 16:46:03 -0700

    bit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to change
+ * @n: first number
+ * @m: second number
+ * Return: number of bits to change
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int j;
+	unsigned long int x = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		j = x >> i;
+		if (j & 1)
+			count++;
+	}
+	return (count);
+}
