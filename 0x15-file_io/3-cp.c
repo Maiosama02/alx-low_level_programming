b0fdd3ea808c9bc2caae4626b3ba29385c625610
Maiosama02 <mai.karoom02@gmail.com>
2023-10-02 12:28:26 -0700

    task0

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFERSIZE 1024
+
+/**
+ * main -  copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int from, to;
+	char buff[BUFFERSIZE];
+	int nread, nwrite;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	from = open(argv[1], O_RDONLY);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(from);
+		exit(99);
+	}
+	while ((nread = read(from, buff, BUFFERSIZE)) > 0)
+	{
+		nwrite = write(to, buff, nread);
+		if (nwrite == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(from);
+			close(to);
+			exit(99);
+		}
+	}
+	if (close(from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		exit(100);
+	}
+	if (close(to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
+		exit(100);
+	}
+	return (0);
+}
